d49fd95866ce7a331fceb7161b6dec6bdfce3f01
Malthe Høj-Sunesen <malthe.s@gmail.com>
2014-02-24 23:17:09 +0100

    Outlining the event detector

--- adc/src/event_detection.cpp
+++ adc/src/event_detection.cpp
@@ -7,6 +7,21 @@ int main() {
 	//get pointer from A/D (recorder.hpp) --mhs
 	//operate on data and determine if event --mhs
 	//UDP package to Perl script --mhs
-	//TODO: Get access to the Perl script (not in git branch as far as I can see) --mhs
+	//TODO: Get access to Perl script (not in git branch what I can see) --mhs
+	//TODO: How high is the highest possible sound (sint, long?) --mhs
+	//discussion: The first 100 samples - ok to be wasted? Want to do 
+	//in order to avoid a boolean check every time the sounds are 
+	//analyzed. Working on like it is a deal. --mhs
+
+	int32 x[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	int32 store[8][100];
+	int32 sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+	uint32 least_sig_val[8];	//address to the least significant (= earliest)
+								//value in the store. --mhs
+
+	for(int i = 0; i < 8; i++) {
+		
+	}
+
 
 }
