58ab59d15d4d30ffac9fb4e2a18044d460e29c67
Malthe Høj-Sunesen <malthe.s@gmail.com>
2014-02-25 22:53:00 +0100

    Outlining the event detector

--- adc/src/event_detection.cpp
+++ adc/src/event_detection.cpp
@@ -1,27 +1,68 @@
 #include <iostream>
+#include <vector>
 //This is mostly a draft document, ought to be merged into control.cpp, for
 //ease of use vis-à-vis pointers
 using namespace std;
 
+#define NUM_DEVICES 20
+#define NUM_MICS	8
+#define SUM			100	//amount of samples in sum --mhs
+#define THRESHOLD	500	//is it a bat or not? --mhs
+
+int insert(int, int);
+void batsound_present(int);
+void batsound_gone(int);
+
+int old_value = 0, new_value = 0;
+int x[NUM_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
+int store[NUM_DEVICES][SUM];	//classic circular array --mhs
+int sum[NUM_DEVICES] = {0, 0, 0, 0, 0, 0, 0, 0};
+//vector for storing which devives are recording already --mhs
+int least_sig_val[NUM_DEVICES];	//address to the least significant (= earliest)
+								//value in the store. --mhs
 int main() {
 	//get pointer from A/D (recorder.hpp) --mhs
 	//operate on data and determine if event --mhs
 	//UDP package to Perl script --mhs
 	//TODO: Get access to Perl script (not in git branch what I can see) --mhs
 	//TODO: How high is the highest possible sound (sint, long?) --mhs
+	//TODO: Best possible circular array implementation? --mhs
 	//discussion: The first 100 samples - ok to be wasted? Want to do 
 	//in order to avoid a boolean check every time the sounds are 
 	//analyzed. Working on like it is a deal. --mhs
 
-	int32 x[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	int32 store[8][100];
-	int32 sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-	uint32 least_sig_val[8];	//address to the least significant (= earliest)
-								//value in the store. --mhs
+	
 
-	for(int i = 0; i < 8; i++) {
-		
+	for(int i = 0; i < NUM_DEVICES; i++) {
+		//get value/sound data from pointer, put int array x[] --mhs
+		old_value = store[i][least_sig_value];
+		new_value = x[i];
+
+		sum[i] = sum[i] - old_value * old_value + new_value * new_value;
+		insert(new_value, i);
+
+		if(sum[i] > THRESHOLD) { //remember to avoid multiple of the same device --mhs
+			batsound_present(i);	//should create new thread to handle this,
+									//to allow bats at more than one location
+									//--mhs
+		}
+	}
+}
+
+int insert(int num, int index) {
+	if(least_sig_value[index] > (SUM - 1)) {
+		least_sig_value[index] = -1;	//intentionally off by one - see just below --mhs
 	}
 
+	least_sig_value[index]++;	//no if-statement to avoid additional overhead --mhs
 
+	store[index][least_sig_value[index]] = num;
+
+	return 1;
+}
+
+void batsound_present(int index) {
+	//UDP to Perl script to start recording --mhs
+	//get value/sound data from pointer for -index- device, put in dedicated 
+	//
 }
